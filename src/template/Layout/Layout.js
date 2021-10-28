86e609b3b57ff9fa9dd965166269c78221e3d713
dovanthao2404 <dovanthao2404@gmail.com>
2021-10-28 11:05:29 +0700

    udpate

--- src/template/Layout/Layout.js
+++ src/template/Layout/Layout.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import SideBar from '../../components/SideBar'
-import Routes from '../../routes/Routes'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import SideBar from "../../components/SideBar";
+import Routes from "../../routes/Routes";
+import { SpreadsheetComponent } from "@syncfusion/ej2-react-spreadsheet";
 
 const Layout = () => {
     return (
         <BrowserRouter>
-            <Route render={props => (
-                <div>
-                    < SideBar {...props} />
-                    <div className='container'>
-                        <Routes />
+            <Route
+                render={(props) => (
+                    <div>
+                        <div style={{ display: "flex" }}>
+                            <SideBar {...props} />
+                            {/* <div className="container"> */}
+                            <SpreadsheetComponent
+                                allowOpen={true}
+                                openUrl="https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open"
+                                // beforeOpen={this.beforeOpen.bind(this)}
+                                allowSave={true}
+                                saveUrl="https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save"
+                            />
+
+                            {/* <Routes /> */}
+                            {/* </div> */}
+                        </div>
                     </div>
-                </div>
-            )} />
-        </BrowserRouter >
+                )}
+            />
+        </BrowserRouter>
+    );
+};
 
-    )
-}
-
-export default Layout
+export default Layout;
